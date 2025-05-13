7e459a3556bd3bb3b15e1fab577bc04b30e1065b
Kumitokiru <161684032+Kumitokiru@users.noreply.github.com>
2025-05-14 07:39:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def on_connect():
     emit("bulk_alerts", alerts)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
